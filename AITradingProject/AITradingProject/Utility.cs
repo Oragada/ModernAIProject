c65f84fb2513ad2022a0dffb408023a81fbd4116
Azurest <daniel1511@live.dk>
2014-10-22 13:23:08 +0200

    added utility class

---
+++ AITradingProject/AITradingProject/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AITradingProject
+{
+    /// <summary>
+    /// Implement any utility function here
+    /// </summary>
+    class Utility
+    {
+        //TODO create a static random object for use by model layer.
+
+    }
+}
