ddcb019441803efda9b8f7727b89d3267139358d
Oragada <Oragada@gmail.com>
2014-11-11 13:16:03 +0100

    Added initial structure for the aptly named LeMasterMind agent

---
+++ AITradingProject/AITradingProject/Agent/LeMasterMindAgent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AITradingProject.Agent.MM_Subsystems;
+using AITradingProjectModel.Model;
+
+namespace AITradingProject.Agent
+{
+    public class LeMasterMindAgent : Agent
+    {
+        public MasterMind mind;
+        public EvalTrade tradeEval;
+        public WtoT whoToTrade;
+        public TradeGenerator tradeBuild;
+        public NEATManager neat;
+
+        public LeMasterMindAgent()
+        {
+            tradeEval = new EvalTrade();
+        }
+
+        public override List<KeyValuePair<int, Dictionary<Resource, int>>> GetOfferProposals(City city)
+        {
+            List<KeyValuePair<int, Dictionary<Resource, int>>> ops = new List<KeyValuePair<int, Dictionary<Resource, int>>>();
+            while (PointsRemaining())
+            {
+                int tradePartner = whoToTrade.GetTradingPartner();
+
+                KeyValuePair<int, Dictionary<Resource, int>> t = tradeBuild.CreateTrade(tradePartner);
+                
+                ops.Add(t);
+            }
+
+            return ops;
+
+
+        }
+
+        private bool PointsRemaining()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EvaluateTrade(Offer offer)
+        {
+            return tradeEval.Evaluate(offer);
+        }
+
+        public override void TradeCompleted(Offer offer, TradeStatus cc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AITradingProject/AITradingProject/Agent/MM-Subsystems/MasterMind.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AITradingProjectModel.Model;
+
+namespace AITradingProject.Agent.MM_Subsystems
+{
+    public class MasterMind
+    {
+        private DecisionTree dt;
+    }
+
+    public class EvalTrade
+    {
+        private ANN neural;
+
+        public bool Evaluate(Offer offer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class WtoT
+    {
+        public abstract int GetTradingPartner();
+    }
+
+    public class TradeGenerator
+    {
+        private ANN neural;
+
+        public KeyValuePair<int, Dictionary<Resource, int>> CreateTrade(int tradePartner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NEATManager
+    {
+        public ANN AdjustNetwork(ANN network, List<TurnLog> results)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TurnLog
+    {
+        private Dictionary<KeyValuePair<int, Dictionary<Resource, int>>, TradeStatus> data;
+
+    }
+
+    internal class DecisionTree
+    {
+    }
+
+    public class ANN
+    {
+    }
+    
+
+}
